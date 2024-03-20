039cec403bb1e1400613ac1363dcd61a26092aaf
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2024-03-20 14:25:01 +0300

    Lesson main 9

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () =>{
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </div>
+    )
+}
+export default Header
