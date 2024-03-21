8cc5c7f637e0d5ec90e78e15a2fc7a5b7c5c7d3e
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2024-03-21 17:14:31 +0300

    Lesson 20 NavLink active 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import React from "react";
+
 
 
 const App = () => {
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,4 +12,4 @@ const Navbar = () => {
         </div>
     )
 }
-export default Navbar
+export default Navbar;
