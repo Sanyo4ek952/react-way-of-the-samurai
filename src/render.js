c8be5d4230d66bd82fb8d340a44481f896e69128
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2024-03-28 14:01:03 +0300

    Lesson 32 Добавление поста часть 2 render

--- src/render.js
+++ src/render.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import {addPost} from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 export let renderEntireTree = (state) => {
-    const root = ReactDOM.createRoot(document.getElementById('root'));
+
     root.render(
         <React.StrictMode>
             <BrowserRouter>
